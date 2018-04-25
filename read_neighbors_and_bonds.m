a36c976ac0a195a31d2abc0a7c73f4910f556ade
yosyp <y.shvab+git@gmail.com>
2018-04-25 12:59:12 -0400

    function to convert <x,y,z> input to bonds/neighbors

---
+++ read_neighbors_and_bonds.m
@@ -0,0 +1,54 @@
+%
+% Author: Yosyp Schwab
+%
+% Function takes x,y,z coordinates and outputs 2 square matrices with
+% length equal to number of atoms. First matrix represents all interatomic
+% distances between all atoms. Second matrix represents nearest neighbors
+% as bonds between atomic pairs.
+%
+% Inputs:
+%        natoms number of atoms <integer>
+%        x x-coordinates of atoms <vector>
+%        y y-coordinates of atoms <vector>
+%        z z-coordinates of atoms <vector>
+% Outputs:
+%       neighbs interatomic distances between all pairs <sq-matrix>
+%       bonds nearest neighbors connected graph <sq-matrix>
+%
+function [neighbs, bonds] = read_neighbors_and_bonds(natoms, x, y, z)
+    for i = 1:natoms
+        for j = 1:natoms
+            if i ~= j
+                neighbs(j,i) = sqrt((x(j)-x(i))^2 + ... 
+                                    (y(j)-y(i))^2 + ... 
+                                    (z(j)-z(i))^2);
+                neighbs(j,i) = round(neighbs(j,i), 4);
+            else
+                neighbs(j,i) = NaN;
+            end
+        end
+    end
+
+    bonds = zeros(natoms,natoms);
+    for i = 1:natoms
+        % Alternative approach using:
+        % nnindex = find(neighbs(i,:) == min(neighbs(i,:)));
+
+        % Sort all neighbors to find first 3 nearest 
+        [out,idx] = sort(neighbs(i,:));
+
+        % In a homogeneus n-ring system only connecting two neighors will
+        % form a fully connected graph
+        for k = 1:2
+            bonds(i,idx(k)) = 1;
+        end
+        
+        % If the system is heterogeneus with 5-6-7-rings, then all three
+        % nearest neighbors are sometimes necessary to form a fully
+        % connected graph. There is a cutoff (10%) to make sure non-nearest
+        % neighbors are not connected.
+        if out(3) < 1.1*out(2)
+            bonds(i,idx(3)) = 1;
+        end
+    end
+end
