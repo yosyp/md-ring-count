c9b104933a5b82f26d69cb956736b82c75bcc60d
yosyp <y.shvab+git@gmail.com>
2018-05-03 08:59:59 -0400

    supercomputer job script, input file

---
+++ code/md-runs/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH -t 00:10:00
+#SBATCH -p eqa-mse_4270_6270-18sp
+#SBATCH -A mse_4270_6270
+#SBATCH --output=output.txt
+
+# Run program
+module load intel
+cd $SLURM_SUBMIT_DIR
+./runmd
